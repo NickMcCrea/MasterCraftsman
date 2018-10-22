adc4f259037e3b2a0afbc4527b6d26e41c2fa866
Creadude <nickmccrea@gmail.com>
2018-10-22 21:39:20 +0100

    Cinemachine camera, indoor / outdoor teleporting, layering in test scene.

---
+++ Assets/Code/TileHider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileHider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<TilemapRenderer>().enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
