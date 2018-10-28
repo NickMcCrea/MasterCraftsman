5933105a4cefc6261c93bc33f3482a2ad0b3fde5
Creadude <nickmccrea@gmail.com>
2018-10-28 20:02:21 +0000

    Simple dialogue box system.

---
+++ Assets/Code/DialogueBoxController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBoxController : MonoBehaviour {
+
+
+
+    private Queue<string> linesToDisplay;
+    Image panel;
+    Text textBox;
+   
+
+
+    // Use this for initialization
+    void Start () {
+
+        panel = gameObject.GetComponent<Image>();
+        textBox = gameObject.GetComponentInChildren<Text>();
+        linesToDisplay = new Queue<string>();
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.Return))
+            NextLine();
+
+	}
+
+
+    public void SetText(string[] lines)
+    {
+        linesToDisplay.Clear();
+        for (int i = lines.Length - 1; i >= 0; i--)
+            linesToDisplay.Enqueue(lines[i]);
+
+    }
+
+    public void OpenDialogue()
+    {
+        gameObject.SetActive(true);
+        NextLine();
+    }
+
+    private void NextLine()
+    {
+        if(linesToDisplay.Count == 0)
+        {
+            return;
+        }
+        
+        textBox.text = linesToDisplay.Dequeue();
+    }
+}
---
+++ Assets/Code/UITest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour {
+
+    const string glyphs = "abcdefghijklmnopqrstuvwxyz 0123456789";
+    DialogueBoxController dbc;
+    // Use this for initialization
+    void Start ()
+    {
+
+
+        dbc = GameObject.Find("DialogueBox").GetComponent<DialogueBoxController>();
+
+        //GameObject.Find("DialogueBox").SetActive(false);
+
+    
+    }
+
+    private void ActivateDialogueTest()
+    {
+        string[] testStrings = new string[10];
+
+        for (int i = 0; i < testStrings.Length; i++)
+        {
+            string s = "";
+            int charAmount = Random.Range(50, 100); //set those to the minimum and maximum length of your string
+            for (int j = 0; j < charAmount; j++)
+            {
+                s += glyphs[Random.Range(0, glyphs.Length)];
+            }
+            testStrings[i] = s;
+        }
+
+
+
+        dbc.SetText(testStrings);
+        dbc.OpenDialogue();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            ActivateDialogueTest();
+
+	}
+}
